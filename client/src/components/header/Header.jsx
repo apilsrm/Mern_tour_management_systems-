ff4635a83099e3b95e3668dc185aa9e8f7fbe2e8
apilsrm <sharmaapil05@gmail.com>
2024-01-23 22:29:37 +0545

    headeradded

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,38 +1,77 @@
-import React from 'react'
-import { Container, Row, Button } from 'reactstrap';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
 
+import logo from "../../assets/images/logo.png";
+import "./Header.css";
 
-
-const nav__links=[
+const nav__links = [
   {
-    path:'/home',
-    display:'Home'
+    path: "/home",
+    display: "Home",
   },
   {
-    path:'#',
-    display:'About'
+    path: "/about",
+    display: "About",
   },
   {
-    path:'/tours',
-    display:'Tours'
+    path: "/tours",
+    display: "Tours",
   },
-
-]
-
+];
 
 const Header = () => {
-  return <header className='header'>
-    <Container>
-      <Row>
-        <div className="nav__wrapper d-flex align-items-center justify-content-between">
-          {/* -----------logo---- */}
-          
-          {/* -----------logo end---- */}
-        </div>
-      </Row>
-    </Container>
-  </header>
-}
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/* -----------logo---- */}
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
 
-export default Header
+            {/* -----------logo end---- */}
+
+            {/* -----------menu start---- */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* -----------menu end---- */}
+
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+
+              <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
